1a789fed905466e26e0a5fec92716844407166ba
lavnyasharma <lavanya2210sharma@gmail.com>
2021-06-17 12:52:01 +0530

    portfolio

---
+++ style.js
@@ -0,0 +1,26 @@
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+$window.on('scroll', check_if_in_view);
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } else {
+            $element.removeClass('in-view');
+        }
+    });
+}
